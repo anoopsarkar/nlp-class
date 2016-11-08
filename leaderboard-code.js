714296467ddb61c4592e1cd717968eedbe71f7c9
Anoop Sarkar <anoop+github@cs.sfu.ca>
2016-11-08 01:46:17 -0800

    hw4 goes live. again.

--- leaderboard-code.js
+++ leaderboard-code.js
@@ -16,10 +16,10 @@ $.ajax({
     success: function () {
         $('#loader').hide();
         // The current assignment number (0-indexed)
-        var assignment_number = 3;
+        var assignment_number = 4;
 
         // The maximum assignment number (0-indexed)
-        var max_assignment_number = 3;
+        var max_assignment_number = 4;
 
         var rows = "";
         var scoreranks = new Array();
