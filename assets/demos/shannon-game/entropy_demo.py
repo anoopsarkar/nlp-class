01e3363215078764c891bf60232cc4d3a8d022ca
Anoop Sarkar <anoop+github@cs.sfu.ca>
2019-09-17 15:49:01 -0700

    updated to py3

--- assets/demos/shannon-game/entropy_demo.py
+++ assets/demos/shannon-game/entropy_demo.py
@@ -1,4 +1,3 @@
-
 #  scp oak.fas.sfu.ca:/cs/natlang-data/austen/austen.lm .
 #  scp oak.fas.sfu.ca:/cs/natlang-data/austen/austen.txt .
 
@@ -12,29 +11,29 @@ import getopt
 import tty
 import termios
 
-from gen_from_lm import *
+from gen_from_lm import LM
 
 log_two_base_ten = math.log10(2)
 
 def log_base_2(base_ten_logprob):
-  return base_ten_logprob / log_two_base_ten
+    return base_ten_logprob / log_two_base_ten
+
 
 def entropy_and_word_prob(events, word, uniform):
-  word_prob = 0.0
-  entropy = 0.0
-  unif = 1.0 / len(events)
-  for (w3, prob) in events:
-    if uniform:
-      entropy += unif * math.log(unif,2)
-    else:
-      entropy += math.pow(10.0, prob) * log_base_2(prob)
-    if (w3 == word): 
-      word_prob = math.pow(10.0, prob)
-  if len(events) == 1:
+    word_prob = 0.0
     entropy = 0.0
-  else:
-    entropy = -1 * entropy
-  return (entropy, word_prob)
+    unif = 1.0 / len(events)
+    for (w3, prob) in events:
+        if uniform:
+            entropy += unif * math.log(unif, 2)
+        else:
+            entropy += math.pow(10.0, prob) * log_base_2(prob)
+        if (w3 == word):
+            word_prob = math.pow(10.0, prob)
+    if len(events) == 1:
+        entropy = 0.0
+    return (entropy, word_prob)
+
 
 def getch():
     fd = sys.stdin.fileno()
@@ -43,85 +42,95 @@ def getch():
         tty.setcbreak(sys.stdin.fileno())
         ch = sys.stdin.read(1)
     except KeyboardInterrupt:
-        print >>sys.stderr, "Control-C pressed"
+        print("Control-C pressed", file=sys.stderr)
         sys.exit(0)
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-def compute_entropy(filename, numsents, numskip, printprob, wait, uniform):
-  """prints entropy of each word position based on probability from a language model"""
 
-  try: 
-    f = open(filename, 'r')
-  except: 
-    usage()
-    sys.exit(2)
+def compute_entropy(lm, filename, numsents, numskip, printprob, wait, uniform):
+    """prints entropy of each word position based on probability from a language model"""
 
-  i = 0
-  n = 0
-  for line in f:
-    i += 1
-    if (i <= numskip): continue
+    try:
+        f = open(filename, 'r')
+    except:
+        usage()
+        sys.exit(2)
 
-    if (n >= numsents): break
-    n += 1
+    i = 0
+    n = 0
+    for line in f:
+        i += 1
+        if (i <= numskip): continue
 
-    line = line.strip()
-    words = line.split()
+        if (n >= numsents): break
+        n += 1
 
-    if len(words) <= 1: continue
+        line = line.strip()
+        words = line.split()
 
-    w1 = '<s>'
-    w2 = words[0]
-    w3 = words[1]
-    (entropy, word_prob) = entropy_and_word_prob(start[(w1, w2)], w3, uniform)
-    print w1, w2
-    if printprob: print w3, entropy, word_prob 
-    else: print w3, entropy
+        if len(words) <= 1: continue
 
-    w1 = w2
-    w2 = w3
-    for w3 in words[2:]:
-      if wait: getch()
-      (entropy, word_prob) = entropy_and_word_prob(lm[(w1, w2)], w3, uniform)
-      if printprob: print w3, entropy, word_prob
-      else: print w3, entropy
-      w1 = w2
-      w2 = w3
-    print
+        w1 = '<s>'
+        w2 = words[0]
+        w3 = words[1]
+        (entropy, word_prob) = entropy_and_word_prob(lm.start[(w1, w2)], w3, uniform)
+        print(w1, w2)
+        if printprob: print("{}\t\t\t[entropy={:.2f} n_gram_prob={:.2f}]".format(w3, entropy, word_prob))
+        else: print("{}\t\t\t[entropy={:.2f}]".format(w3, entropy))
+
+        w1 = w2
+        w2 = w3
+        for w3 in words[2:]:
+            if wait: getch()
+            (entropy,
+             word_prob) = entropy_and_word_prob(lm.lm[(w1, w2)], w3, uniform)
+            if printprob: print("{}\t\t\t[entropy={:.2f} n_gram_prob={:.2f}]".format(w3, entropy, word_prob))
+            else: print("{}\t\t\t[entropy={:.2f}]".format(w3, entropy))
+            w1 = w2
+            w2 = w3
+        print()
+
 
 def usage():
-  print >> sys.stderr, "usage: python entropy_demo.py -w|--wait -p|--printprob -u|--uniform -l|--lmfile file -f|--corpusfile file -n|--numsents num -s|--skip num"
+    print(
+        "usage: python entropy_demo.py -w|--wait -p|--printprob -u|--uniform -l|--lmfile file -f|--corpusfile file -n|--numsents num -s|--skip num",
+        file=sys.stderr)
+
 
 if __name__ == '__main__':
-  try:
-    (lmfile, corpusfile, numsents, numskip, printprob, wait, uniform) = (None, None, 1, 0, 0, 0, 0)
-    opts, args = getopt.getopt(sys.argv[1:], "wpul:f:n:s:", ["wait", "printprob", "uniform", "lmfile=", "corpusfile=", "numsents=", "skip="])
-  except getopt.GetoptError:
-    usage()
-    sys.exit(2)
+    try:
+        (lmfile, corpusfile, numsents, numskip, printprob, wait,
+         uniform) = (None, None, 1, 0, 0, 0, 0)
+        opts, args = getopt.getopt(sys.argv[1:], "wpul:f:n:s:", [
+            "wait", "printprob", "uniform", "lmfile=", "corpusfile=",
+            "numsents=", "skip="
+        ])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
-  for o, a in opts:
-    if o in ("-w", "--wait"):
-      wait = 1
-    if o in ("-p", "--printprob"):
-      printprob = 1
-    if o in ("-u", "--uniform"):
-      uniform = 1
-    if o in ("-l", "--lmfile"):
-      lmfile = a
-    if o in ("-f", "--corpusfile"):
-      corpusfile = a
-    if o in ("-n", "--numsents"):
-      numsents = string.atoi(a)
-    if o in ("-s", "--skip"):
-      numskip = string.atoi(a)
+    for o, a in opts:
+        if o in ("-w", "--wait"):
+            wait = 1
+        if o in ("-p", "--printprob"):
+            printprob = 1
+        if o in ("-u", "--uniform"):
+            uniform = 1
+        if o in ("-l", "--lmfile"):
+            lmfile = a
+        if o in ("-f", "--corpusfile"):
+            corpusfile = a
+        if o in ("-n", "--numsents"):
+            numsents = int(a)
+        if o in ("-s", "--skip"):
+            numskip = int(a)
 
-  if (corpusfile == None):
-    usage()
-    sys.exit(2)
+    if (corpusfile == None):
+        usage()
+        sys.exit(2)
 
-  load_lm(lmfile)
-  compute_entropy(corpusfile, numsents, numskip, printprob, wait, uniform)
-
+    lm = LM()
+    lm.load_lm(lmfile)
+    compute_entropy(lm, corpusfile, numsents, numskip, printprob, wait, uniform)
--- assets/demos/shannon-game/gen_from_lm.py
+++ assets/demos/shannon-game/gen_from_lm.py
@@ -1,4 +1,3 @@
-
 import sys
 import string
 import time
@@ -7,132 +6,143 @@ import math
 import getopt
 import gzip
 
-start = {} # dictionary that holds trigram logprobs for valid starts
-           # for a sentence
-lm = {} # dictionary that holds the rest of the trigram logprobs
+class LM:
 
-# Warning: the language model created by SRILM should not delete low
-# count ngrams, otherwise this generator will not work. Use a command
-# like:
-#
-# ngram-count -order 3 -gtmax 0 -gt3min 1 -text austen.txt -lm
-# austen.lm
+    def __init__(self):
+        self.start = {}  # dictionary that holds trigram logprobs for valid starts
+        # for a sentence
+        self.lm = {}  # dictionary that holds the rest of the trigram logprobs
 
-# for trigram probability, prob = p(w3 | w1, w2) we create a
-# dictionary where the key is the tuple (w1,w2) and the value is a
-# list: [ (w3-1, prob-1) , (w3-2, prob-2), ... ]
-#
-# we have two such dictionaries, one for cases where w1 = <s> so that
-# we can use this dictionary to start the sentence, and the rest of
-# the ngrams are stored in the dictionary lm
+        # Warning: the language model created by SRILM should not delete low
+        # count ngrams, otherwise this generator will not work. Use a command
+        # like:
+        #
+        # ngram-count -order 3 -gtmax 0 -gt3min 1 -text austen.txt -lm
+        # austen.lm
 
-def load_lm(file):
-  """load_lm loads a file in the ARPA LM format used by SRILM"""
-  if file[-3:] == '.gz':
-    lmfile = gzip.open(file, 'rb')
-  else:
-    lmfile = open(file, 'r')
-  begin = False
-  for line in lmfile:
-    line = string.rstrip(line)
-    if line[:5] == '\\end\\':
-      break
-    if line == "":
-      continue
-    if begin == True:
-      fields = line.split()
-      if fields[1] == "<s>":
-        startkey = (fields[1], fields[2])
-        startvalue = (fields[3], string.atof(fields[0]))
-        if start.has_key(startkey): start[startkey].append(startvalue)
-        else: start[startkey] = [ startvalue ]
-      else:
-        key = (fields[1], fields[2])
-        value = (fields[3], string.atof(fields[0]))
-        if lm.has_key(key): lm[key].append(value)
-        else: lm[key] = [ value ]
-    if line[:9] == '\\3-grams:':
-      begin = True
-  lmfile.close()
+        # for trigram probability, prob = p(w3 | w1, w2) we create a
+        # dictionary where the key is the tuple (w1,w2) and the value is a
+        # list: [ (w3-1, prob-1) , (w3-2, prob-2), ... ]
+        #
+        # we have two such dictionaries, one for cases where w1 = <s> so that
+        # we can use this dictionary to start the sentence, and the rest of
+        # the ngrams are stored in the dictionary lm
 
-def generate(numsents, printprob):
-  """generates numsents sentences by random sampling from a language model"""
-  start_opt_list = start.keys()
-  while numsents > 0:
-    random.seed()
 
-    # generate the start of the sentence by starting with a random
-    # <s>, w1, w2 trigram
-    (w1, w2) = random.choice(start_opt_list)
-    next_word_list = start[(w1, w2)]
-    (w3, prob) = random.choice(next_word_list)
-
-    # the sentence and probability of each word is stored in gen_sent
-    # and gen_logprob
-    gen_sent = [ w1, w2 ] # the sentence starts with w1, w2
-    gen_logprob = []
-
-    # loop until end of sentence </s> is generated
-    while 1:
-      gen_sent.append(w3)
-      gen_logprob.append(prob) # prob = p(w3 | w1, w2)
-
-      if w3 == "</s>": break
-
-      prob_out = random.random()
-      accumulate = 0.0
-      key = (w2, w3)
-      w2 = w3
-
-      if not lm.has_key(key):
-        print >> sys.stderr, "missing key", key
-        break
-
-      # main loop: sample from the events { w3-1, w3-2, ... } using
-      # the prob = p(w3-i | w1, w2)
-      for (lm_w, lm_logprob) in lm[key]:
-
-        if lm_logprob > -98:
-          lm_prob = math.exp(lm_logprob)
+    def load_lm(self, filename):
+        """load_lm loads a file in the ARPA LM format used by SRILM"""
+        if filename[-3:] == '.gz':
+            lmfile = gzip.open(filename, 'rt')
         else:
-          lm_prob = 0.0
+            lmfile = open(filename, 'r')
+        begin = False
+        for line in lmfile:
+            line = line.rstrip()
+            if line[:5] == '\\end\\':
+                break
+            if line == "":
+                continue
+            if begin:
+                fields = line.split()
+                if fields[1] == "<s>":
+                    startkey = (fields[1], fields[2])
+                    startvalue = (fields[3], float(fields[0]))
+                    if startkey in self.start: self.start[startkey].append(startvalue)
+                    else: self.start[startkey] = [startvalue]
+                else:
+                    key = (fields[1], fields[2])
+                    value = (fields[3], float(fields[0]))
+                    if key in self.lm: self.lm[key].append(value)
+                    else: self.lm[key] = [value]
+            if line[:9] == '\\3-grams:':
+                begin = True
+        lmfile.close()
 
-        w3 = lm_w
-        prob = lm_logprob
 
-        if (prob_out < (lm_prob + accumulate)):
-          break
-        else:
-          accumulate += lm_prob
+    def generate(self, numsents, printprob):
+        """generates numsents sentences by random sampling from a language model"""
+        start_opt_list = list(self.start.keys())
+        print(start_opt_list)
+        while numsents > 0:
+            random.seed()
 
-    print " ".join(gen_sent)
-    if (printprob):
-      print " ".join([ "%f" % i for i in gen_logprob])
-    numsents -= 1
+            # generate the start of the sentence by starting with a random
+            # <s>, w1, w2 trigram
+            (w1, w2) = random.choice(start_opt_list)
+            next_word_list = self.start[(w1, w2)]
+            (w3, prob) = random.choice(next_word_list)
+
+            # the sentence and probability of each word is stored in gen_sent
+            # and gen_logprob
+            gen_sent = [w1, w2]  # the sentence starts with w1, w2
+            gen_logprob = []
+
+            # loop until end of sentence </s> is generated
+            while 1:
+                gen_sent.append(w3)
+                gen_logprob.append(prob)  # prob = p(w3 | w1, w2)
+
+                if w3 == "</s>": break
+
+                prob_out = random.random()
+                accumulate = 0.0
+                key = (w2, w3)
+                w2 = w3
+
+                if key not in self.lm:
+                    print("missing key", key, file=sys.stderr)
+                    break
+
+                # main loop: sample from the events { w3-1, w3-2, ... } using
+                # the prob = p(w3-i | w1, w2)
+                for (lm_w, lm_logprob) in self.lm[key]:
+
+                    if lm_logprob > -98:
+                        lm_prob = math.exp(lm_logprob)
+                    else:
+                        lm_prob = 0.0
+
+                    w3 = lm_w
+                    prob = lm_logprob
+
+                    if (prob_out < (lm_prob + accumulate)):
+                        break
+                    else:
+                        accumulate += lm_prob
+
+            print(" ".join(gen_sent))
+            if (printprob):
+                print(" ".join(["%f" % i for i in gen_logprob]))
+            numsents -= 1
+
 
 def usage():
-  print >> sys.stderr, "usage: python gen-from-lm.py [-p|--printprob] -l|--lmfile file -n|--numsents num"
+    print(
+        "usage: python gen-from-lm.py [-p|--printprob] -l|--lmfile file -n|--numsents num",
+        file=sys.stderr)
+
 
 if __name__ == '__main__':
-  try:
-    (printprob, lmfile, numsents) = (0, None, 1)
-    opts, args = getopt.getopt(sys.argv[1:], "pl:n:", ["printprob", "lmfile=", "numsents="])
-  except getopt.GetoptError:
-    usage()
-    sys.exit(2)
+    try:
+        (printprob, lmfile, numsents) = (0, None, 1)
+        opts, args = getopt.getopt(sys.argv[1:], "pl:n:",
+                                   ["printprob", "lmfile=", "numsents="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
-  for o, a in opts:
-    if o in ("-p", "--printprob"):
-      printprob = 1
-    if o in ("-l", "--lmfile"):
-      lmfile = a
-    if o in ("-n", "--numsents"):
-      numsents = string.atoi(a)
+    for o, a in opts:
+        if o in ("-p", "--printprob"):
+            printprob = 1
+        if o in ("-l", "--lmfile"):
+            lmfile = a
+        if o in ("-n", "--numsents"):
+            numsents = int(a)
 
-  if (lmfile == None):
-    usage()
-    sys.exit(2)
+    if (lmfile == None):
+        usage()
+        sys.exit(2)
 
-  load_lm(lmfile)
-  generate(numsents, printprob)
-
+    lm = LM()
+    lm.load_lm(lmfile)
+    lm.generate(numsents, printprob)
