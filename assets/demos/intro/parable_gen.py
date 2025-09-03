16f70d8b88ffd48052708cf78f4789e3735c4b65
Anoop Sarkar <193767+anoopsarkar@users.noreply.github.com>
2025-09-03 10:11:22 -0700

    add data for ensegment

--- assets/demos/intro/parable_gen.py
+++ assets/demos/intro/parable_gen.py
@@ -9,17 +9,18 @@ if torch.backends.mps.is_available():
     device = torch.device("mps")
 
 access_token=os.environ.get("HF_ACCESS_TOKEN")
-model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-#model_id = "Qwen/Qwen2-1.5B-Instruct"
+model_id = "Qwen/Qwen3-4B-Instruct-2507"
+#model_id = "meta-llama/Llama-3.2-3B-Instruct"
+
 pipe = pipeline(
     "text-generation",
     device=device,
     model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
+    model_kwargs={"dtype": torch.bfloat16},
     token=access_token,
 )
 prompt = 'Computers will never'
-full_prompt = f"Finish this reassuring parable about the superiority of humans over computers in a short sentence: {prompt} ____."
+full_prompt = f"Finish this reassuring parable about the superiority of humans over computers in a sentence with 10 words using creativity and humor: {prompt} ____."
 messages = [
     {
         "role": "system",
@@ -31,11 +32,13 @@ messages = [
     },
 ]
 
+print(prompt)
 for _ in range(10):
     outputs = pipe(
         messages,
-        temperature=0.9,
-        max_new_tokens=64,
+        temperature=1.0,
+        max_new_tokens=128,
         do_sample=True,
+        pad_token_id=pipe.tokenizer.eos_token_id,
     )
-    print(prompt, outputs[0]["generated_text"][-1]["content"].split("\n", 1)[0])
+    print(outputs[0]["generated_text"][-1]["content"].split("\n", 1)[0])
